e53ffe698e6e9ea14168f2e15f3be0be396d15a4
bagama ilyasov <bagamailyasov@mail.ru>
2020-08-22 17:02:30 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Bagama Ilyasov on 22.08.2020.
+//  Copyright © 2020 Bagama Ilyasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
